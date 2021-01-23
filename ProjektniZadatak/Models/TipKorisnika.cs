a5a95119b20185cd0f61e0ccdd9946c3d26b750e
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-23 15:21:01 +0100

    Added tipe of user

---
+++ ProjektniZadatak/Models/TipKorisnika.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektniZadatak.Models
+{
+    public class TipKorisnika
+    {
+        private ImeTipaKorisnika tip;
+        private int popust;
+        private int potrebniBrojBodova;
+
+        public TipKorisnika()
+        {
+        }
+
+        public TipKorisnika(ImeTipaKorisnika tip, int popust, int potrebniBrojBodova)
+        {
+            this.Tip = tip;
+            this.Popust = popust;
+            this.PotrebniBrojBodova = potrebniBrojBodova;
+        }
+
+        public ImeTipaKorisnika Tip { get => tip; set => tip = value; }
+        public int Popust { get => popust; set => popust = value; }
+        public int PotrebniBrojBodova { get => potrebniBrojBodova; set => potrebniBrojBodova = value; }
+    }
+}
