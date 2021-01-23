6822de5d6f3a71f2a0b2f47204d39f798e070a22
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-23 16:07:16 +0100

    Added function for saving user in database

--- ProjektniZadatak/Models/DataKorisnik.cs
+++ ProjektniZadatak/Models/DataKorisnik.cs
@@ -23,11 +23,26 @@ namespace ProjektniZadatak.Models
                 string[] token = line.Split(';');
                 Korisnik k = new Korisnik(token[0], token[1], token[2], token[3], (PolKorisnika)Enum.Parse(typeof(PolKorisnika), token[4]), DateTime.Parse(token[5]), (UlogaKorisnika)Enum.Parse(typeof(UlogaKorisnika), token[6]));
                 korisnici.Add(k);
-                if(token[6] == "PRODAVAC")
-                {
-
-                }
             }
+            sr.Close();
+            stream.Close();
+
+            return korisnici;
         }
+
+
+        public static void SaveUser(Korisnik k)
+        {
+            string path = HostingEnvironment.MapPath("~/App_Data/korisnici.txt");
+            FileStream stream = new FileStream(path, FileMode.Append);
+            StreamWriter sw = new StreamWriter(stream);
+
+            sw.WriteLine(k.KorisnickoIme + ";" + k.Lozinka + ";" + k.Ime + ";" + k.Prezime + ";" + k.Pol.ToString() + ";" + k.DatumRodjenja.ToString() + ";" + k.Uloga.ToString());
+
+            sw.Close();
+            stream.Close();
+        }
+
+
     }
 }
---
+++ ProjektniZadatak/Models/Prodavac.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektniZadatak.Models
+{
+    public class Prodavac
+    {
+
+    }
+}
