d3bb1c8274a4edf03790202477d0d5ba24e1bae7
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-23 15:14:43 +0100

    Added txt document where will be all users

---
+++ ProjektniZadatak/Models/Korisnici.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ProjektniZadatak.Models
+{
+    public class Korisnici
+    {
+        static List<Korisnik> korisnici = (List<Korisnik>)HttpContext.Current.Application["korisnici"];
+
+        public static void Update(Korisnik korisnik)
+        {
+            string path = HostingEnvironment.MapPath("~/App_Data/korisnici.txt");
+            FileStream stream = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(stream);
+
+            foreach(Korisnik k in korisnici)
+            {
+                sw.WriteLine(k.KorisnickoIme + ";" + k.Lozinka + ";" + k.Ime + ";" + k.Prezime + ";" + k.Pol.ToString() + ";" + k.DatumRodjenja.ToString() + ";" + k.Uloga.ToString());
+            }
+
+            sw.Close();
+            stream.Close();
+        }
+    }
+}
