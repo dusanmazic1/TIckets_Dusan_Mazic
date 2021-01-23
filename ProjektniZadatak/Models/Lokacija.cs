ccb80b973effb927b9457751aa8f0586ddd40fa9
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-23 15:37:07 +0100

    Added location

---
+++ ProjektniZadatak/Models/Lokacija.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektniZadatak.Models
+{
+    public class Lokacija
+    {
+        private double gografskaDuzina;
+        private double gografskaSirina;
+        private MestoOdrzavanja mestoOdrzavanja;
+
+        public Lokacija(MestoOdrzavanja mestoOdrzavanja) 
+        {
+            var adress = mestoOdrzavanja.Ulica + " " + mestoOdrzavanja.Broj + ", " + mestoOdrzavanja.Grad + ", " + mestoOdrzavanja.PostanskiBroj;
+            this.MestoOdrzavanja = mestoOdrzavanja;
+        }
+
+        public double GografskaDuzina { get => gografskaDuzina; set => gografskaDuzina = value; }
+        public double GografskaSirina { get => gografskaSirina; set => gografskaSirina = value; }
+        public MestoOdrzavanja MestoOdrzavanja { get => mestoOdrzavanja; set => mestoOdrzavanja = value; }
+    }
+}
---
+++ ProjektniZadatak/Models/MestoOdrzavanja.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektniZadatak.Models
+{
+    public class MestoOdrzavanja
+    {
+        private string ulica;
+        private string broj;
+        private string grad;
+        private long postanskiBroj;
+
+        public MestoOdrzavanja()
+        {
+        }
+
+        public MestoOdrzavanja(string ulica, string broj, string grad, long postanskiBroj)
+        {
+            this.Ulica = ulica;
+            this.Broj = broj;
+            this.Grad = grad;
+            this.PostanskiBroj = postanskiBroj;
+        }
+
+        public string Ulica { get => ulica; set => ulica = value; }
+        public string Broj { get => broj; set => broj = value; }
+        public string Grad { get => grad; set => grad = value; }
+        public long PostanskiBroj { get => postanskiBroj; set => postanskiBroj = value; }
+    }
+}
