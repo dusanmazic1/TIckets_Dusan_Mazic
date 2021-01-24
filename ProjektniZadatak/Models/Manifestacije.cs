6728476a571540457a001d0ab7eee49344c8da54
Dusan Mazic <dusanmazic123@gmail.com>
2021-01-24 22:24:18 +0100

    added prodavac and added manifestacije

---
+++ ProjektniZadatak/Models/Manifestacije.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace ProjektniZadatak.Models
+{
+    public class Manifestacije
+    {
+        static List<Manifestacija> manifestacije = (List<Manifestacija>)HttpContext.Current.Application["manifestacije"];
+
+        public static List<Manifestacija> Update(Manifestacija manifestacija)
+        {
+            string path = HostingEnvironment.MapPath("~/App_Data/manifestacije.txt");
+            FileStream stream = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(stream);
+
+            foreach(Manifestacija m in manifestacije)
+            {
+                sw.WriteLine(m.Naziv + ";" + m.TipManifestacije.ToString() + ";" + m.PreostalaMesta + ";" + m.Vreme.ToString() + ";" + m.CenaRegular.ToString() + ";" + m.CenaFunPit.ToString() + ";" + m.CenaVIP.ToString() + ";" + m.Status.ToString() + ";" + m.Mesto.Ulica + ";" + m.Mesto.Broj + ";" + m.Mesto.Grad + ";" + m.Mesto.PostanskiBroj.ToString() + ";" + m.Prodavac.Korisnik.KorisnickoIme + ";" + m.PreostalaMesta.ToString());
+            }
+
+            sw.Close();
+            stream.Close();
+        }
+    }
+}
--- ProjektniZadatak/Models/Prodavac.cs
+++ ProjektniZadatak/Models/Prodavac.cs
@@ -7,6 +7,20 @@ namespace ProjektniZadatak.Models
 {
     public class Prodavac
     {
+        private List<Manifestacija> manifestacije;
+        private Korisnik korisnik;
 
+        public Prodavac()
+        {
+        }
+
+        public Prodavac(Korisnik korisnik)
+        {
+            this.Korisnik = korisnik;
+            Manifestacije = new List<Manifestacija>();
+        }
+
+        public List<Manifestacija> Manifestacije { get => manifestacije; set => manifestacije = value; }
+        public Korisnik Korisnik { get => korisnik; set => korisnik = value; }
     }
 }
